a6584eee006a33f2dc46ee815f4b62c1e9c5eae9
satyam sahoo <satyamelement@gmail.com>
2018-06-23 18:25:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $(".para1").click(function(){
+        $(".hide1").slideToggle("slow", function(){
+
+        });
+    });
+});
+
+$(document).ready(function(){
+    $(".para2").click(function(){
+        $(".hide2").slideToggle("slow", function(){
+
+        });
+    });
+});
+
+$(document).ready(function(){
+    $(".para3").click(function(){
+        $(".hide3").slideToggle("slow", function(){
+
+        });
+    });
+});
